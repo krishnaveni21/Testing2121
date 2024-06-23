b839a4bba9ceb5abb0a3d40a8c8666c869c5d81f
krishna <krishnaveni.kommana@gmail.com>
2024-06-23 12:29:50 -0500

    first commit

---
+++ src/main/java/com/test/Testing2121/Testing2121Application.java
@@ -0,0 +1,13 @@
+package com.test.Testing2121;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testing2121Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testing2121Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/Testing2121/Testing2121ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.Testing2121;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testing2121ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
