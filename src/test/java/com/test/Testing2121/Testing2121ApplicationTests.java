8de44496bb36a09b1ddd3395b23f4d7269692fbe
krishna <krishnaveni.kommana@gmail.com>
2024-06-23 12:45:08 -0500

    Thrid commit

--- src/test/java/com/test/Testing2121/Testing2121ApplicationTests.java
+++ src/test/java/com/test/Testing2121/Testing2121ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.test.Testing2121;
+/*package com.test.Testing2121;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class Testing2121ApplicationTests {
 	void contextLoads() {
 	}
 
-}
+}*/
